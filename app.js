69e0556143a53d1a42e251532735dbfa03736b21
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-04-24 17:00:36 +0900

    day0424

--- app.js
+++ app.js
@@ -2,11 +2,36 @@
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
 
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+// function onLoginBtnClick() {
+//     // console.log("hello",loginInput.value);
+//     // console.log("click!!!!");
+//     const username = loginInput.value;
+//     if(username === "") {
+//         alert("Please write your name");
+//     } else if(username.length>15) {
+//         alert("Your name is too long.")
+//     }   
+// }
+
+//loginButton.addEventListener("click", onLoginBtnClick);
+
+
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-function onLoginBtnClick() {
-    console.log("hello",loginInput.value);
-    console.log("click!!!!");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(tomato) {
+    // const username = loginInput.value;
+    tomato.preventDefault();
+    console.log(tomato);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function handleLinkClick(){
+    alert("clicked!");
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
